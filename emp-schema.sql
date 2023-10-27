1b6142c0a0d91afa9236d96f9aece3e1e0b6cc7b
Sabarish <avsabarish@gmail.com>
2023-10-27 21:17:13 +0530

    add-emp updated

---
+++ emp-schema.sql
@@ -0,0 +1,1 @@
+CREATE TABLE `bzk8ffarhyqpmvdgdhxn`.`user` (`id` INT NOT NULL , `first_name` VARCHAR(45) NOT NULL , `last_name` VARCHAR(45) NOT NULL , `email` VARCHAR(45) NOT NULL , `phone` VARCHAR(45) NOT NULL , `comments` TEXT NOT NULL , `status` VARCHAR(10) NOT NULL DEFAULT 'Active' ) ENGINE = InnoDB;
